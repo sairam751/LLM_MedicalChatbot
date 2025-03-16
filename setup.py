ab7ffce0c5d855b37abd21dc7b8a9962e06107c6
sairam751 <sairam751348@gmail.com>
2025-03-16 19:14:57 +0530

    setup as local package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.1',
+    author= 'sai ram reddy',
+    author_email= 'sairam751348@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
